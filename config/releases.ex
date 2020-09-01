96739f78e68b4425e098ca6603fb95209d3814d7
Matt Willy <matt.willy@gmail.com>
2020-09-01 14:26:16 -0600

    setup the cluster config

--- config/releases.ex
+++ config/releases.ex
@@ -1,7 +1,28 @@
 import Config
 
+name = "matt"
+
 config :nook_book, NookBookWeb.Endpoint,
   server: true,
   http: [port: 4000],
-  url: [host: "localhost"],
+  url: [host: "#{name}.nookbook.online"],
   secret_key_base: "08Gm1PGaG2HVAj+Jp1wnbVHLCabyigRvQwWhfYbQhesoNKzCRxR9pWMsyPpHKJ20"
+
+config :libcluster,
+  topologies: [
+    nook_book: [
+      strategy: Cluster.Strategy.Epmd,
+      config: [
+        hosts: [
+          :"nook_book@10.0.1.115",
+          :"nook_book@10.0.1.53"
+        ]
+      ]
+    ]
+  ]
+
+config :nook_book,
+  cluster_role:
+    System.get_env("CLUSTER_ROLE", "member")
+    |> String.to_atom(),
+  base_uri: "http://#{name}.nookbook.online"
