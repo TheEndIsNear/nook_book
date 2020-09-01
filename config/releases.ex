d97c823d1b3e63d03bc27862cddbd10ff451b436
Matt Willy <matt.willy@gmail.com>
2020-09-01 13:24:14 -0600

    generate a release

---
+++ config/releases.ex
@@ -0,0 +1,7 @@
+import Config
+
+config :nook_book, NookBookWeb.Endpoint,
+  server: true,
+  http: [port: 4000],
+  url: [host: "localhost"],
+  secret_key_base: "08Gm1PGaG2HVAj+Jp1wnbVHLCabyigRvQwWhfYbQhesoNKzCRxR9pWMsyPpHKJ20"
